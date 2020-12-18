0d1fb0199a572e3d7e0c9f48cf697224f3f5b5cd
Karen-Stewart <kazstewart911@hotmail.com>
2020-12-18 13:05:40 +1000

    fixing post

--- src/controllers/post_controller.py
+++ src/controllers/post_controller.py
@@ -7,12 +7,13 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from services.auth_services import verify_user
 from models.Post import Post
 from sqlalchemy.sql import func, label
+from sqlalchemy.orm import joinedload
 
 posts = Blueprint("post", __name__, url_prefix="/post")
 
 @posts.route("/", methods=["GET"])
 def post_index():
-    post = Post.query.all()
+    post = db.session.query(Post).order_by(Post.post_name).all()
     #return jsonify(posts_schema.dump(post))
     return render_template("home_page.html", posts= post)
 
@@ -53,40 +54,42 @@ def post_create(user=None):
 
     return jsonify(post_schema.dump(new_post))
 
-# @profiles.route("/<string:username>", methods=["GET"])
-# def profiles_show(username):
-#     #Return a single user
-#     profile = Profiles.query.filter_by(username = username).first()
-#     return jsonify(profile_schema.dump(profile))
+@posts.route("/<string:post_name>", methods=["GET"])
+def post_show(post_name):
+    #Return a single user
+    post = Post.query.filter_by(post_name = post_name).first()
+    return jsonify(post_schema.dump(post))
 
-# @profiles.route("/<string:username>", methods=["PUT", "PATCH"])
-# @jwt_required
-# @verify_user
-# def profiles_update(username, user=None):
-#     #Update a user
-#     profile = Profiles.query.filter_by(username = username, user_id=user.id)
-#     profile_fields = profile_schema.load(request.json)
+@posts.route("/<string:post_name>", methods=["PUT", "PATCH"])
+@jwt_required
+@verify_user
+def post_update(post_name, user=None):
+    #Update a user
+    post = Post.query.filter_by(post_name = post_name)
+    post_fields = post_schema.load(request.json)
 
-#     if profile.count() != 1:
-#         return abort(401, description="Unauthorised to update this user")
-#     profile.update(profile_fields)
+    if post.count() != 1:
+        return abort(401, description="Unauthorised to update this user")
+    post.update(post_fields)
 
 
-#     db.session.commit()
+    db.session.commit()
 
-#     return jsonify(profile_schema.dump(profile[0]))
+    return jsonify(post_schema.dump(post[0]))
 
-# @profiles.route("/<string:username>", methods=["DELETE"])
-# @jwt_required
-# @verify_user
-# def profiles_delete(username, user=None):
-#     #Delete a User
-#     profile = Profiles.query.filter_by(username=username, user_id=user.id).first()
+@posts.route("/<int:id>", methods=["DELETE"])
+@jwt_required
+@verify_user
+def post_delete(user, postid):
+    #Delete a User
+    #post = Post.query.filter_by(post_name=post_name).first()
+    post = Post.query.options(joinedload("profile")).filter_by(postid = postid, profile_id = user.id).first()
+    #post = db.session.query(Post).filter(Post.post_name == post_name).first()
 
-#     if not profile:
-#         return abort(400, description="Unauthorised to delete user")
+    if not post:
+        return abort(400, description="Unauthorised to delete user")
 
-#     db.session.delete(profile)
-#     db.session.commit()
+    db.session.delete(post)
+    db.session.commit()
 
-#     return jsonify(profile_schema.dump(profile))
+    return jsonify(post_schema.dump(post))
